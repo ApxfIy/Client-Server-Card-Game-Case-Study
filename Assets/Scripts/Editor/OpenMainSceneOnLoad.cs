2c2023adf461b9a4d86e00b9c9f302a5d7d1279e
Apxfly <apxfly1@gmail.com>
2026-04-29 11:20:03 +0200

    Make sure Main scene loads on start up

---
+++ Assets/Scripts/Editor/OpenMainSceneOnLoad.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Editor
+{
+    [InitializeOnLoad]
+    public static class OpenMainSceneOnLoad
+    {
+        static OpenMainSceneOnLoad()
+        {
+            EditorApplication.delayCall += () =>
+            {
+                if (EditorSceneManager.GetActiveScene().name == "Untitled")
+                {
+                    EditorSceneManager.OpenScene("Assets/Scenes/Main.unity");
+                }
+            };
+        }
+    }
+}
